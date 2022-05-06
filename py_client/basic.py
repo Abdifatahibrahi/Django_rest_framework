acf50417b5275de30ba3e59298c6e48e42500daa
Abdifatah <alsidiq099@gmail.com>
2022-05-06 22:27:21 +0300

    created new app api and added more functionalities

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,4 +1,10 @@
 import requests
 
-endpoint = "https://httpbin.org/status/200"
-endpoint = "https://httpbin.org/"
+# endpoint = "https://httpbin.org/status/200"
+endpoint = "http://127.0.0.1:8000/api/"
+get_response = requests.get(endpoint)
+
+print(get_response.text)
+print(get_response.status_code)
+
+print(get_response.json()['message'])
