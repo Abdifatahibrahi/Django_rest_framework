32a26e52b649a4e1c5ab7d6213d9affeee686adb
Abdifatah <alsidiq099@gmail.com>
2022-05-08 18:30:44 +0300

    changed the response from JsonResponse to Resonse

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,16 @@
 from pyexpat import model
 from django.shortcuts import render
-from django.http import JsonResponse
+# from django.http import JsonResponse
 from products.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.forms.models import model_to_dict
 
-
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
     data = {}
     model_data = Product.objects.all().order_by('?').first()
 
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-    return JsonResponse(data)
+        data = model_to_dict(model_data)
+    return Response(data)
